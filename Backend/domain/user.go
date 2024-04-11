f8eaefe8223478e43aed00f51161233b8928c272
fasika <fasikafikadu38@gmail.com>
2024-04-11 22:49:27 +0300

    [backend] feat: add database connectio

--- Backend/domain/user.go
+++ Backend/domain/user.go
@@ -1,16 +1,32 @@
 package domain
 
-import "github.com/jinzhu/gorm"
+import (
+    "github.com/jinzhu/gorm"
+)
+
+// UserRole custom type for user roles
+type UserRole string
+
+const (
+    Teacher UserRole = "teacher"
+    Student UserRole = "student"
+    Admin   UserRole = "admin"
+)
 
 // User model
 type User struct {
     gorm.Model
-    Username string `json:"username" binding:"required"`
-    Password string `json:"password" binding:"required"`
-    Email    string `json:"email" binding:"required"`
-    Role     string `json:"role" binding:"required" gorm:"type:ENUM('teacher', 'student', 'admin')"`
-    Topics   []Topic
-    Notes    []Note
+    Username    string      `json:"username" binding:"required"`
+    Password    string      `json:"password" binding:"required"`
+    Email       string      `json:"email" binding:"required"`
+    Role        UserRole    `json:"role" binding:"required" gorm:"type:user_role"`
+    Topics      []Topic
+    Notes       []Note
     Experiments []Experiment
-    Quizzes  []Quiz
+    Quizzes     []Quiz
+}
+
+// TableName specifies the table name for the User model
+func (User) TableName() string {
+    return "users"
 }
---
+++ Backend/persistence/migrations/db.go
@@ -0,0 +1,37 @@
+package migrations
+
+import (
+    "os"
+    "sync"
+
+    "github.com/joho/godotenv"
+    "gorm.io/gorm"
+    "gorm.io/driver/postgres"
+    "hackathon.com/leariningApp/domain"
+)
+
+var db *gorm.DB
+var once sync.Once
+
+func GetDB() *gorm.DB {
+    once.Do(func() {
+        // Load environment variables from .env file
+        err := godotenv.Load()
+        if err != nil {
+            panic("Error loading .env file")
+        }
+
+        // Get the database URL from the environment
+        dbURL := os.Getenv("DATABASE_URL")
+
+        // Connect to the database
+        db, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+        if err != nil {
+            panic("Failed to connect to database!")
+        }
+
+        // Auto-migrate the model
+        db.AutoMigrate(&domain.User{}, &domain.Topic{}, &domain.Experiment{}, &domain.Quiz{})
+    })
+    return db
+}
