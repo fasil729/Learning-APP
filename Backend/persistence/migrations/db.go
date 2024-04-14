d926d8309594bcf2d39758bcfc118fff9c28df08
Ephrem <eph21sz@gmail.com>
2024-04-14 23:43:22 +0300

    create generic_repository

--- Backend/persistence/migrations/db.go
+++ Backend/persistence/migrations/db.go
@@ -1,37 +1,37 @@
 package migrations
 
 import (
-    "os"
-    "sync"
+	"os"
+	"sync"
 
-    "github.com/joho/godotenv"
-    "gorm.io/gorm"
-    "gorm.io/driver/postgres"
-    "hackathon.com/leariningApp/domain"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"hackathon.com/leariningApp/domain"
 )
 
 var db *gorm.DB
 var once sync.Once
 
 func GetDB() *gorm.DB {
-    once.Do(func() {
-        // Load environment variables from .env file
-        err := godotenv.Load()
-        if err != nil {
-            panic("Error loading .env file")
-        }
+	once.Do(func() {
+		// Load environment variables from .env file
+		err := godotenv.Load()
+		if err != nil {
+			panic("Error loading .env file")
+		}
 
-        // Get the database URL from the environment
-        dbURL := os.Getenv("DATABASE_URL")
+		// Get the database URL from the environment
+		dbURL := os.Getenv("DATABASE_URL")
 
-        // Connect to the database
-        db, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
-        if err != nil {
-            panic("Failed to connect to database!")
-        }
+		// Connect to the database
+		db, err = gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+		if err != nil {
+			panic("Failed to connect to database!")
+		}
 
-        // Auto-migrate the model
-        db.AutoMigrate(&domain.User{}, &domain.Topic{}, &domain.Experiment{}, &domain.Quiz{})
-    })
-    return db
+		// Auto-migrate the model
+		db.AutoMigrate(&domain.User{}, &domain.Topic{}, &domain.Experiment{}, &domain.Quiz{})
+	})
+	return db
 }
