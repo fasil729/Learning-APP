600530325825c10a8a5def7f121de0bb09bb7650
Ephrem <eph21sz@gmail.com>
2024-04-20 21:40:01 +0300

    Add ContentLink field to Note model

--- Backend/domain/note.go
+++ Backend/domain/note.go
@@ -5,8 +5,6 @@ import "github.com/jinzhu/gorm"
 // Note model
 type Note struct {
 	gorm.Model
-	NoteContent string `json:"note_content" binding:"required"`
-	ContentType string `json:"content_type" binding:"required"`
-	UserID      uint   `json:"-"`
-	TopicID     uint   `json:"-"`
+	ChapterID   uint
+	ContentLink string
 }
