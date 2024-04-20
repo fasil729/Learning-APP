5e26add142ddb6968807a2d12a1565548b810d4f
dagmawitensay <dagitensay@gmail.com>
2024-04-20 20:08:40 +0300

    [frontend] add shared components

---
+++ frontend_web/components/home/home.tsx
@@ -0,0 +1,11 @@
+import SideBar from "../shared/sideBar/sideBar";
+import TopHeader from "../shared/top_header/top_header";
+
+export default function HomePage() {
+    return <div className="">
+            <SideBar/>
+            <div className="ml-[18%]">
+                <TopHeader/>
+            </div>
+        </div>
+}
