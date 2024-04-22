f9d7ba6805f038ad0a930136ebd82df3e974915d
fasika <fasikafikadu38@gmail.com>
2024-04-22 22:19:32 +0300

    [backend] update host from localhost to remote

--- Backend/docs/docs.go
+++ Backend/docs/docs.go
@@ -932,7 +932,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://learning-app-idt8.onrender.com/",
+	Host:             "learning-app-idt8.onrender.com",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Brilliant Learning APP API",
