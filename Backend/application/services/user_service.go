97f6afcdbd2d19022afba05bac34ac9338093907
fasika <fasikafikadu38@gmail.com>
2024-04-23 00:09:23 +0300

    [backend] update user regestration

--- Backend/application/services/user_service.go
+++ Backend/application/services/user_service.go
@@ -23,9 +23,6 @@ func (service *UserService) RegisterUser(dto *dtos.RegisterDTO) (*dtos.UserDTO, 
 
 	// Check if there is already a user with the given username or email
 	existingUser, err := service.userRepository.FindByUsernameOrEmail(dto.Username, dto.Email)
-	if err != nil {
-		return nil, err
-	}
 	if existingUser != nil {
 		return nil, errors.New("username or email already exists")
 	}
