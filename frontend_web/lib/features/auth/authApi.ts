a7974cc0611cc09c8c794ce9d62ac96a3581f9f3
natnaeldejenekebede <natnaeldejene19@gmail>
2024-04-24 22:18:25 +0300

    auth

--- frontend_web/lib/features/auth/authApi.ts
+++ frontend_web/lib/features/auth/authApi.ts
@@ -1,29 +1,48 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:4000/'
-})
+// export const baseQuery = fetchBaseQuery({
+//     baseUrl: 'http://localhost:4000/'
+// });
 
-export const authApi = createApi({
-    baseQuery,
-    endpoints: (builder) => ({
-        signUp: builder.mutation({
-            query: () => ''
-        }),
+// export const authApi = createApi({
+//     reducerPath: 'authApi',
+//     baseQuery: baseQuery,
+//     endpoints: (builder) => ({
+//         signUp: builder.mutation({
+//             query: (userData) => ({
+//                 url: 'api/signup',
+//                 method: 'POST',
+//                 body: userData,
+//             }),
+//         }),
+//         login: builder.mutation({
+//             query: (loginData) => ({
+//                 url: 'api/login',
+//                 method: 'POST',
+//                 body: loginData,
+//             }),
+//         }),
+//         logout: builder.mutation({
+//             query: () => ({
+//                 url: 'api/logout',
+//                 method: 'POST',
+//             }),
+//         }),
+//         getAuthData: builder.query({
+//             query: (token) => ({
+//                 url: 'api/auth-details',
+//                 method: 'GET',
+//                 headers: {
+//                     Authorization: `Bearer ${token}`,
+//                 },
+//             }),
+//         }),
+//     }),
+// });
 
-        login: builder.mutation({
-            query: () => ''
-        }),
-
-        logout: builder.mutation({
-            query: () => ''
-        })
-    })
-})
-
-
-export const {
-    useSignUpMutation,
-    useLoginMutation,
-    useLogoutMutation
-} = authApi;
+// export const {
+//     useSignUpMutation,
+//     useLoginMutation,
+//     useLogoutMutation,
+//     useGetAuthDataQuery
+// } = authApi;
