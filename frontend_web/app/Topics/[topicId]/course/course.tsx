87df139fc6ad9a1495c1e2fe94b3e23da4f3bf4b
dagmawitensay <dagitensay@gmail.com>
2024-05-01 00:38:31 +0300

    [frontend] add lesson detail

--- frontend_web/app/Topics/[topicId]/course/course.tsx
+++ frontend_web/app/Topics/[topicId]/course/course.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react'
 import {
   Tabs,
@@ -12,31 +13,36 @@ import Community from './community';
 import VideoComponent from '../../../../components/video';
 import { CiLock } from "react-icons/ci";
 import CourseDescribe from './courseDescribe';
+import { useSelector } from 'react-redux';
 const CourseDetailComponent = () => {
+  const {isSuccess, isLoading, errors, data} = useSelector((state: any) => state.subjects.topic);
+  console.log(data);
+
+  console.log(data);
   return (<div className="bg-[#e5e5e5] min-h-screen w-full flex xl:p-20 gap-2 lg:justify-center">
 
-    <div className="w-full  lg:w-8/12  flex   justify-center px-10">
+    <div className="w-full  flex   justify-center">
 
       <div className="space-y-6  min-h-screen w-full">
 
-        <div className="space-y-2">
+        {/* <div className="space-y-2">
           <h1 className='font-semibold text-lg'>Discover</h1>
           <div className="flex gap">
             <p className='text-gray-600 flex'><span>Course</span> <span className='p-2'><FaChevronRight size={12} /></span></p>
             <h2 className='text-gray-800 font-semibold'>Artificial Intelligence & Machine Learning</h2>
           </div>
-        </div>
+        </div> */}
 
         <div className="bg-white flex  flex-col justify-center items-center ">
           {/* <div className="flex justify-center p-4 w-full md:w-11/12 xl:w-10/12" >
             <VideoComponent />
           </div> */}
 
-          <h1 className='font-semibold text-lg'>Artificial Intelligence & Machine Learning</h1>
-          <p className='text-gray-600 text-sm'>By  <span className='text-gray-800 text-base'>Simon Shaw</span>, Illustrator and 3D designer</p>
+          <h1 className='font-semibold text-lg'>{data.Subject?.Name ?? "Test topic"}</h1>
+          {/* <p className='text-gray-600 text-sm'>By  <span className='text-gray-800 text-base'>Simon Shaw</span>, Illustrator and 3D designer</p> */}
 
           <div className="py-10 w-full">
-            <Content/>
+            <Content isLoading={isLoading} errors={errors} data={data}/>
             {/* <Tabs defaultValue="information" className="w-full">
               <TabsList className="grid w-full grid-cols-2 md:grid-cols-4 gap-2 px-6">
                 <TabsTrigger value="information" className="data-[state=active]:bg-[#FF7800] data-[state=active]:text-white rounded-full">Information</TabsTrigger>
--- frontend_web/lib/utils.ts
+++ frontend_web/lib/utils.ts
@@ -1,6 +1,16 @@
 import { type ClassValue, clsx } from "clsx"
+import { cookies } from "next/headers";
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const token = cookies().get("accessToken");
+export async function getTopicDetail(lessonName: string) {
+  const res = await fetch(`https://learning-app-idt8.onrender.com/lessons/54/content`, { headers: {
+    Authorization: `Bearer ${token}`
+  }});
+  console.log(res)
+  return res.body;
 }
