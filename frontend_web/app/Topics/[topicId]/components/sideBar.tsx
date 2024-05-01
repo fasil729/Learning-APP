f14a9bb9f1e3e6deaedfec17af921045e9c806c3
Habtamu <habtetsegaye19@gmail.com>
2024-05-01 23:07:52 +0300

    side bar bug is fixed

--- frontend_web/app/Topics/[topicId]/components/sideBar.tsx
+++ frontend_web/app/Topics/[topicId]/components/sideBar.tsx
@@ -21,10 +21,10 @@ import { useDispatch, useSelector } from "react-redux";
 export const TopicSideBar  = () => {
     const {isSuccess, isLoading, errors, data} = useSelector((state: any) => state.subjects.topic);
     
-    return <aside className="fixed overflow-y-scroll top-0 bg-gradient-to-b from-[#8fb7dd] to-[#c88fe9] left-0 w-[26%] h-screen px-4 py-8 border-white rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700">
-            <a href="#" className="mx-auto">
+    return <aside className="fixed overflow-y-scroll top-0 bg-gradient-to-b from-[#ddebf5] to-[#dedae0] left-0 w-[26%] h-screen px-4 py-8 border-white rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 mt-[100px]">
+            {/* <a href="#" className="mx-auto">
                 <img className="w-auto h-6 sm:h-7 text-white" src="https://merakiui.com/images/full-logo.svg" alt=""/>
-            </a>
+            </a> */}
             <Accordion type="single" collapsible className="w-full  mt-[100px]">
             {data.Chapters && data.Chapters.map((chapter: any, index: number) => {
               return <AccordionItem key={index} value={`${index}`} className="text-start">
